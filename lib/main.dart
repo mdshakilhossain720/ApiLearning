fb7685bbbc8b720c2704cdc2f5fa16b8b675138c
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-04-06 17:33:37 +0600

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main(){
+import 'views/splash_screen.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      
+      home: SplashScreen(),
     );
   }
 }
